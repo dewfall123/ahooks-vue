01c76f0e33dac2945824993d66903cfe6e07df5d
zuofenghua <11100776@bbktel.com>
2020-09-16 21:24:23 +0800

    sync

--- packages/vhooks/src/index.ts
+++ packages/vhooks/src/index.ts
@@ -9,4 +9,4 @@ export { useSize } from './hooks/useSize';
 export { useWorkerFunction } from './hooks/useWorkerFunction';
 export { useThrottle } from './hooks/useThrottle';
 export { usePage } from './hooks/usePage';
-export * from './hooks/useOLAP';
+export { useOLAP } from './hooks/useOLAP';
