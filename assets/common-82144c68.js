3b1327ca0a05f943cf2d209222a684d7dde45d0f
zuofenghua <11100776@bbktel.com>
2020-10-22 18:53:46 +0800

    Updates

---
+++ assets/common-82144c68.js
@@ -0,0 +1,1 @@
+import{u as t}from"./common-8f5b8761.js";const e=(e=!1)=>{const{state:s,toggle:o}=t(e);return{state:s,toggle:()=>o(),setTrue:()=>o(!0),setFalse:()=>o(!1)}};export{e as u};
--- assets/common-b73e72e6.js
+++
@@ -1,1 +0,0 @@
-import{x as t}from"./common-a14a00e4.js";function e(e=!1,o){const a=t(e),s=t=>{a.value=t},i=void 0===o?!e:o;return{state:a,toggle:t=>{void 0===t&&(t=a.value===e?i:e),s(t)},setLeft:()=>{s(e)},setRight:()=>{s(i)}}}export{e as u};
