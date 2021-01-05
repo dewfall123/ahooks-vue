838056e33d933deb99db631b9bc22e2ce90822f1
zuofenghua <11100776@bbktel.com>
2021-01-05 11:06:49 +0800

    Updates

--- assets/index.ee32534f.js
+++
@@ -1,1 +0,0 @@
-import{b as i,q as e,G as t}from"./framework.f47b9a5e.js";function n(){const n=i(document.visibilityState),s=e((()=>"visible"===n.value));function a(){n.value=document.visibilityState}return document.addEventListener("visibilitychange",a),t((()=>{document.removeEventListener("visibilitychange",a)})),{visibilityState:n,isVisible:s}}export{n as u};
