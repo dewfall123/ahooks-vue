93013b931bcb056ab882f386a5bc82a167a35308
zuofenghua <11100776@bbktel.com>
2021-11-25 20:18:58 +0800

    fix: vue2 onMounted bug

--- src/utils/lifeCircle.ts
+++ src/utils/lifeCircle.ts
@@ -2,7 +2,7 @@ import { getCurrentInstance, onMounted } from 'vue-demi';
 
 export function safeOnMounted(hook: () => any) {
   const instance = getCurrentInstance();
-  if (instance?.isMounted) {
+  if (instance?.isMounted || (instance as any)._isMounted) {
     hook();
   } else {
     onMounted(hook);
